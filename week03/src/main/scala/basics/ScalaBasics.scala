962b96912746d84e54c5deef222812a22b727608
Jade Dickinson <jadedickinson+bbk@gmail.com>
2017-02-05 17:11:54 +0000

    Remove more immutable and mutable variables

--- week03/src/main/scala/basics/ScalaBasics.scala
+++ week03/src/main/scala/basics/ScalaBasics.scala
@@ -134,10 +134,7 @@ object ScalaBasics {
    */
   def splitInHalf(s: String): (String, String) = {
     if (s.length % 2 == 0) {
-      var firstHalf = s.substring(0, s.length/2)
-      var secondHalf = s.substring(s.length/2, s.length)
-      val twoHalves = (firstHalf, secondHalf)
-      twoHalves
+      (s.substring(0, s.length/2), s.substring(s.length/2, s.length))
     }
     else {
       var firstHalf = s.substring(0, s.length/2)
