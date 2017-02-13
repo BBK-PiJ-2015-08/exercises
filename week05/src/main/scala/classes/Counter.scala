010d0c1d8476819cc54fbb3a4f69c6019adbb291
Jade Dickinson <jadedickinson@gmail.com>
2017-02-13 20:00:05 +0000

    Reimplement as case class and set default value of count to 0. Tests pass.

--- week05/src/main/scala/classes/Counter.scala
+++ week05/src/main/scala/classes/Counter.scala
@@ -3,7 +3,7 @@ package classes
 /**
   * Created by jade on 08/02/2017.
   */
-class Counter(val count: Int) {
+case class Counter(val count: Int = 0) {
 
   def inc(defaultInc: Int = 1) = new Counter(count + defaultInc)
 
