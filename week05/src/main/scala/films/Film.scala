ea1867b4ce11763dc6243a58b87f7cb330bbaa0e
Jade Dickinson <jadedickinson@gmail.com>
2017-02-16 19:37:53 +0000

    Add type annotations to all three methods

--- week05/src/main/scala/films/Film.scala
+++ week05/src/main/scala/films/Film.scala
@@ -4,11 +4,11 @@ package films
   * Created by jade on 16/02/2017.
   */
 class Film(val name: String, val yearOfRelease: Int, val imdbRating: Double, val director: Director) {
-  def directorsAge = yearOfRelease - director.yearOfBirth
+  def directorsAge: Int = yearOfRelease - director.yearOfBirth
 
-  def isDirectedBy(director: Director) =  director == this.director
+  def isDirectedBy(director: Director): Boolean =  director == this.director
 
-  def copy(name: String = name, yearOfRelease: Int = yearOfRelease, imdbRating: Double = imdbRating, director: Director = director) = {
+  def copy(name: String = name, yearOfRelease: Int = yearOfRelease, imdbRating: Double = imdbRating, director: Director = director): Film = {
     new Film(name, yearOfRelease, imdbRating, director)
   }
 }
