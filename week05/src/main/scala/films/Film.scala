0555dbc069af0a11e80857357e7ad157d17b0bc2
Jade Dickinson <jadedickinson@gmail.com>
2017-02-16 14:18:55 +0000

    Adjust constructor - FilmTest seems to accept it fine

--- week05/src/main/scala/films/Film.scala
+++ week05/src/main/scala/films/Film.scala
@@ -3,12 +3,7 @@ package films
 /**
   * Created by jade on 16/02/2017.
   */
-class Film {
-  val name: String
-  val yearOfRelease: Int
-  val imdbRating: Double
-  val director: Director
-
+class Film(val name: String, val yearOfRelease: Int, val imdbRating: Double, val director: Director) {
   def directorsAge = yearOfRelease - director.yearOfBirth
 
   def isDirectedBy(director: Director) =  director == this.director
