9732263ed909eddb90f3cd47be24939e0d55de75
Jade Dickinson <jadedickinson@gmail.com>
2017-02-16 14:18:03 +0000

    Adjust constructor - code in FilmTest accepts it fine

--- week05/src/main/scala/films/Director.scala
+++ week05/src/main/scala/films/Director.scala
@@ -3,10 +3,6 @@ package films
 /**
   * Created by jade on 16/02/2017.
   */
-abstract class Director {
-  val firstName: String
-  val lastName: String
-  val yearOfBirth: Int
-
+class Director(val firstName: String, val lastName: String, val yearOfBirth: Int) {
   def name = firstName + lastName
 }
