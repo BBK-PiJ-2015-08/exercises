a1fa3741d5b5d1450822ba12087b76e6adc9daea
Jade Dickinson <jadedickinson@gmail.com>
2017-02-16 19:32:26 +0000

    Implement apply method for Director

--- week05/src/main/scala/films/Director.scala
+++ week05/src/main/scala/films/Director.scala
@@ -6,3 +6,11 @@ package films
 class Director(val firstName: String, val lastName: String, val yearOfBirth: Int) {
   def name = firstName + lastName
 }
+
+object Director {
+  def apply(firstName: String, lastName: String, yearOfBirth: Int) = {
+    new Director(firstName, lastName, yearOfBirth)
+  }
+
+
+}
