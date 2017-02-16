e43a6ad55db69fb4dcc1cba9592501a9f35bf0b6
Jade Dickinson <jadedickinson@gmail.com>
2017-02-16 14:12:06 +0000

    Add fields and methods for Film class

--- week05/src/main/scala/films/Film.scala
+++ week05/src/main/scala/films/Film.scala
@@ -4,5 +4,13 @@ package films
   * Created by jade on 16/02/2017.
   */
 class Film {
+  val name: String
+  val yearOfRelease: Int
+  val imdbRating: Double
+  val director: Director
+
+  def directorsAge = yearOfRelease - director.yearOfBirth
+
+  def isDirectedBy(director: Director) =  director == this.director
 
 }
