7462b7336d5c8bb47e238685a8580e1aead1f4c3
Jade Dickinson <jadedickinson@gmail.com>
2017-02-16 19:34:50 +0000

    Add type annotation to name method

--- week05/src/main/scala/films/Director.scala
+++ week05/src/main/scala/films/Director.scala
@@ -4,7 +4,7 @@ package films
   * Created by jade on 16/02/2017.
   */
 class Director(val firstName: String, val lastName: String, val yearOfBirth: Int) {
-  def name = firstName + lastName
+  def name: String = firstName + lastName
 }
 
 object Director {
