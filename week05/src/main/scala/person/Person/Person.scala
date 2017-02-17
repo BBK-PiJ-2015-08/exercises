80bc295ebb5213cc8d774f848892da181da90c02
Jade Dickinson <jadedickinson@gmail.com>
2017-02-17 09:26:40 +0000

    Implement companion object for class with alternate apply method

--- week05/src/main/scala/person/Person/Person.scala
+++ week05/src/main/scala/person/Person/Person.scala
@@ -4,5 +4,12 @@ package person
   * Created by jade on 17/02/2017.
   */
 class Person(firstName: String, lastName: String) {
-  
+
+}
+
+object Person {
+  def apply(fullName: String): Person = {
+    val parts = fullName.split(" ")
+    new Person(parts(0), parts(1))
+  }
 }
