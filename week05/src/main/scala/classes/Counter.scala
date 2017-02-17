9fcf46848b875c106422d649497a8fdc9285a9ae
Jade Dickinson <jadedickinson@gmail.com>
2017-02-17 09:11:42 +0000

    Add type annotations to inc and dec methods

--- week05/src/main/scala/classes/Counter.scala
+++ week05/src/main/scala/classes/Counter.scala
@@ -5,7 +5,8 @@ package classes
   */
 case class Counter(val count: Int = 0) {
 
-  def inc(defaultInc: Int = 1) = new Counter(count + defaultInc)
+  def inc(defaultInc: Int = 1): Counter = new Counter(count + defaultInc)
 
-  def dec(defaultInc: Int = 1) = new Counter(count - defaultInc)
+  def dec(defaultInc: Int = 1): Counter = new Counter(count - defaultInc)
+
 }
