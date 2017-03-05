fe88a05f2ce9c79add4f510cd9ba0a3d0cc59d6f
Jade Dickinson <jadedickinson@gmail.com>
2017-03-05 15:49:33 +0000

    Replace use of BigWheel xz String with the ’s’ variable. Output of test still matches that expected

--- week08/src/main/scala/bridge/BigWheel.scala
+++ week08/src/main/scala/bridge/BigWheel.scala
@@ -1,9 +1,9 @@
 package bridge
 
 case class BigWheel(product: Product, s: String) extends Car(product, s) {
-  override def assemble: Unit = println("Assembling " + product.productName + " for BigWheel xz model")
+  override def assemble: Unit = println("Assembling " + product.productName + " for " + s)
 
-  override def produceProduct: Unit = println("Producing " + product.productName + "\nModifying product" + product.productName + " according to BigWheel xz model")
+  override def produceProduct: Unit = println("Producing " + product.productName + "\nModifying product " + product.productName + " according to " + s)
 
-  override def printDetails: Unit = println("Car: BigWheel xz model, Product:" + product.productName)
+  override def printDetails: Unit = println("Car: " + s + ", Product:" + product.productName)
 }
