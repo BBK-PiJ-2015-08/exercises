a791c687deefe1ba08bc8416d649c0f5d40b71e4
Jade Dickinson <jadedickinson@gmail.com>
2017-03-05 15:45:10 +0000

    Implement BigWheel. Output from test matches that expected

--- week08/src/main/scala/bridge/BigWheel.scala
+++ week08/src/main/scala/bridge/BigWheel.scala
@@ -1,11 +1,9 @@
 package bridge
 
-// TODO
-
 case class BigWheel(product: Product, s: String) extends Car(product, s) {
-  override def assemble: Unit = ???
+  override def assemble: Unit = println("Assembling " + product.productName + " for BigWheel xz model")
 
-  override def produceProduct: Unit = ???
+  override def produceProduct: Unit = println("Producing " + product.productName + "\nModifying product" + product.productName + " according to BigWheel xz model")
 
-  override def printDetails: Unit = ???
+  override def printDetails: Unit = println("Car: BigWheel xz model, Product:" + product.productName)
 }
