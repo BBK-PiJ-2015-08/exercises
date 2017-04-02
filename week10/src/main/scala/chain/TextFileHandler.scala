cdc85791c6fb56fcf596177417be8688f96b0910
Jade Dickinson <jadedickinson@gmail.com>
2017-04-02 18:23:31 +0100

    Implement TextFileHandler methods

--- week10/src/main/scala/chain/TextFileHandler.scala
+++ week10/src/main/scala/chain/TextFileHandler.scala
@@ -1,9 +1,24 @@
 package chain
 
 case class TextFileHandler(s: String) extends Handler {
-  override def setHandler(handler: Handler): Unit = ???
+  var handler: Handler = null
 
-  override def process(file: File): Unit = ???
+  override def setHandler(handler: Handler): Unit = {
+    this.handler = handler
+  }
 
-  override def getHandlerName(): String = ???
+  override def process(file: File): Unit = {
+    if (handler == null) {
+      println("File not supported")
+    } else {
+      if (file.fileType == "text") {
+        println("Process and saving " + file.fileType + "file... by " + getHandlerName())
+      } else {
+        println("Text Handler forwards request to " + handler.getHandlerName())
+        handler.process(file)
+      }
+    }
+  }
+
+  override def getHandlerName(): String = s
 }
