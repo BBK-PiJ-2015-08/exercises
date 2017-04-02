877e11b1d3f51a23feac0ab09e8824761f91335f
Jade Dickinson <jadedickinson@gmail.com>
2017-04-02 18:23:57 +0100

    Implement DocFileHandler methods

--- week10/src/main/scala/chain/DocFileHandler.scala
+++ week10/src/main/scala/chain/DocFileHandler.scala
@@ -1,9 +1,24 @@
 package chain
 
 case class DocFileHandler(s: String) extends Handler {
-  override def setHandler(handler: Handler): Unit = ???
+  var handler: Handler = null
 
-  override def process(file: File): Unit = ???
+  override def setHandler(handler: Handler): Unit = {
+    this.handler = handler
+  }
 
-  override def getHandlerName(): String = ???
+  override def process(file: File): Unit = {
+    if (handler == null) {
+      println("File not supported")
+    } else {
+      if (file.fileType == "doc") {
+        println("Process and saving " + file.fileType + "file... by " + getHandlerName())
+      } else {
+        println("Doc Handler forwards request to " + handler.getHandlerName())
+        handler.process(file)
+      }
+    }
+}
+
+  override def getHandlerName(): String = s
 }
