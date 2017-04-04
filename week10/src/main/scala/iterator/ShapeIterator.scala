32b59a0bbb6254bc54d1f4167ffa4704c3c78095
Jade Dickinson <jadedickinson@gmail.com>
2017-04-04 13:55:58 +0100

    Implement hasNext() and next() methods

--- week10/src/main/scala/iterator/ShapeIterator.scala
+++ week10/src/main/scala/iterator/ShapeIterator.scala
@@ -3,10 +3,19 @@ package iterator
 import java.util.Iterator
 
 class ShapeIterator(private var shapes: Array[Shape]) extends Iterator[Shape] {
+  var current: Int = 0
 
-  override def hasNext(): Boolean = ???
+  override def hasNext(): Boolean = if (shapes.length > current) true else false
 
-  override def next(): Shape = ???
+  override def next(): Shape = {
+    if (hasNext()) {
+      val store = shapes(current)
+      current = current + 1
+      store
+    } else {
+      throw new IndexOutOfBoundsException
+    }
+  }
 
   override def remove(): Unit = ???
 }
