17665f8606fc308a87e1972dd3462306d4fadedc
Jade Dickinson <jadedickinson@gmail.com>
2017-04-04 15:42:28 +0100

    Implement getPlatformInstance(). Test seems to give a good output.

--- week11/src/main/scala/flyweight/PlatformFactory.scala
+++ week11/src/main/scala/flyweight/PlatformFactory.scala
@@ -1,13 +1,20 @@
 package flyweight
 
-import java.util.HashMap
+import java.util
 import java.util.Map
 
 object PlatformFactory {
 
-  private var map: Map[String, Platform] = ???
+  private var map: Map[String, Platform] = new util.HashMap[String, Platform]()
 
-  def getPlatformInstance(platformType: String): Platform = ???
+  def getPlatformInstance(platformType: String): Platform = {
+    platformType match {
+      case "C" => new CPlatform();
+      case "JAVA" => new JavaPlatform();
+      case "RUBY" => new RubyPlatform();
+      case "SCALA" => new ScalaPlatform();
+    }
+  }
   // this method will probably need to be synchronized
 
 }
