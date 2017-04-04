766319a7e1e1df0a0ae493abe24def05286c73a4
Jade Dickinson <jadedickinson@gmail.com>
2017-04-04 15:20:49 +0100

    Interpreter

--- week10/src/main/scala/interpreter/ExpressionUtils.scala
+++ week10/src/main/scala/interpreter/ExpressionUtils.scala
@@ -2,7 +2,13 @@ package interpreter
 
 object ExpressionUtils {
 
-  def isOperator(s: String): Boolean = ???
+  def isOperator(s: String): Boolean = ??? //{
+//    s match {
+//      case "+" =>
+//      case "*" =>
+//      case "-" =>
+//    }
+//  }
 
   def getOperator(s: String, left: Expression, right: Expression): Expression = ???
 }
