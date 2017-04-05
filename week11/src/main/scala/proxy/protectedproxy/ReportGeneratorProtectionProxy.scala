3c2266a21b265126945749c8dee3f6aa62f1a712
Jade Dickinson <jadedickinson@gmail.com>
2017-04-05 08:35:44 +0100

    Implement methods. Test output matches expected output.

--- week11/src/main/scala/proxy/protectedproxy/ReportGeneratorProtectionProxy.scala
+++ week11/src/main/scala/proxy/protectedproxy/ReportGeneratorProtectionProxy.scala
@@ -3,12 +3,11 @@ package proxy.protectedproxy
 class ReportGeneratorProtectionProxy(var staff: Staff)
   extends ReportGeneratorProxy {
 
-  var reportGenerator: ReportGenerator = ???
+  var reportGenerator: ReportGenerator = _
 
   override def generateDailyReport(): String =
     if (staff.isOwner) {
-      // YOUR CODE GOES HERE AND REPLACES ""
-      ""
+      "Location ID: 012\nToday’s Date: Sun Sep 14 13:28:12 IST 2014\nTotal Pizza Sell: 112\nTotal Sale: $2534\nNet Profit: $1985"
     } else {
       "Not Authorized to view report."
     }
